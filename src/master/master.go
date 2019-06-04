56be9d5ee1b34664e6f5e0f15d04b7396bc14de7
tpps88206 <cool96482@hotmail.com>
2019-06-04 14:11:37 +0800

    Change port number

--- src/master/master.go
+++ src/master/master.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    "io/ioutil"
-    "log"
-    "net/http"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", GetData).Methods("GET")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", GetData).Methods("GET")
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":39000", router))
 }
 
 func GetData(w http.ResponseWriter, r *http.Request) {
-    resp, _ := http.Get("http://user.default.svc.cluster.local:8080")
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    response := []byte(string(body))
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(response)
+	resp, _ := http.Get("http://user.default.svc.cluster.local:39000")
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	response := []byte(string(body))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
-
--- src/user/user.go
+++ src/user/user.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", GetUser).Methods("GET")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", GetUser).Methods("GET")
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":39000", router))
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-    var res = map[string]string{"name":"Michael", "email":"michael@gmail.com"}
-    response, _ := json.Marshal(res)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(response)
+	var res = map[string]string{"name": "Michael", "email": "michael@gmail.com"}
+	response, _ := json.Marshal(res)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
-
